7456ea873bf2a531ddcd855c7a37eb85ab4c2a42
Clément <clementpoirie.pro@gmail.com>
2023-11-08 17:10:04 +0100

    création du main et mise en place de fonctions

--- Reconaissance_faciale/face_reconnaissance.py
+++ Reconaissance_faciale/face_reconnaissance.py
@@ -1,105 +1,128 @@
+# ----------------------------------------------------------------------------
+# Facial Recognition using face recognition library
+# ----------------------------------------------------------------------------
+# Description: This script performs real-time facial recognition using the
+# OpenCV and face_recognition libraries. It loads known faces from a directory,
+# captures video from a webcam, and recognizes and labels detected faces in the
+# video stream.
+# ----------------------------------------------------------------------------
+# Author: Clément Poirié
+# Date: 24/10/2023
+# Version: 1.0
+# ----------------------------------------------------------------------------
+# Usage:
+# - Make sure to have the 'face_recognition', 'cv2', and 'numpy' libraries
+#   installed.
+# - Set up a directory with known faces for recognition in the 'known_faces'
+#   directory.
+# - Run the script and it will display the video stream with recognized faces.
+# ----------------------------------------------------------------------------
+# References:
+# - https://github.com/ageitgey/face_recognition/blob/master/examples/facerec_from_webcam_faster.py
+
 import face_recognition
 import cv2
 import numpy as np
 import os
 
+# ----------------------------------------------------------------------------
+# This function initializes a list of known faces and their corresponding names
+# ----------------------------------------------------------------------------
+# No parameters
+# Returns the lists of known faces and their names
 
-KNOWN_FACES_DIR = 'known_faces'
-known_faces = []
-known_names = []
+def init_known_faces():
 
-video_capture = cv2.VideoCapture(0)
+    # Directory containing images of known faces
+    KNOWN_FACES_DIR = 'Reconaissance_faciale/known_faces'
 
-for name in os.listdir(KNOWN_FACES_DIR):
-    for filename in os.listdir(f'{KNOWN_FACES_DIR}/{name}'):
+    # Initialize lists to store face encodings and corresponding names
+    known_faces = []
+    known_names = []
 
-        # Load an image
-        image = face_recognition.load_image_file(f'{KNOWN_FACES_DIR}/{name}/{filename}')
+    # Iterate through subdirectories in the KNOWN_FACES_DIR directory
+    for name in os.listdir(KNOWN_FACES_DIR):
+        for filename in os.listdir(f'{KNOWN_FACES_DIR}/{name}'):
 
-        # Get 128-dimension face encoding
-        # Always returns a list of found faces, for this purpose we take first face only (assuming one face per image as you can't be twice on one image)
-        encoding = face_recognition.face_encodings(image)[0]
+            # Load an image
+            image = face_recognition.load_image_file(f'{KNOWN_FACES_DIR}/{name}/{filename}')
 
-        # Append encodings and name
-        known_faces.append(encoding)
-        known_names.append(name)
+            # Get the 128-dimensional face encoding
+            encoding = face_recognition.face_encodings(image)[0]
 
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
+            # Append encodings and names to their respective lists
+            known_faces.append(encoding)
+            known_names.append(name)
 
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+    return (known_faces, known_names)
 
-    # Only process every other frame of video to save time
+
+# ----------------------------------------------------------------------------
+# Function for real-time facial recognition processing
+# ----------------------------------------------------------------------------
+# 5 parameters
+#   - video --> The video stream captured from the camera. It can be defined as:
+#     video_capture = cv2.VideoCapture(0) to capture video from the default camera (index 0).
+#   - face_locations --> A list that stores the locations of detected faces in a frame.
+#     It can be defined as: face_locations = []
+#   - face_encodings --> A list that holds the 128-dimensional face encodings of the detected faces.
+#     These encodings are used to compare detected faces with known faces for recognition.
+#     It can be defined as: face_encodings = []
+#   - known_faces, known_names --> lists that contains the face encodings of known faces, and the names.
+#     These parameters are init_known_faces function object
+#     It can be defined as: known_faces, known_names  = init_known_faces()
+# Void function no return
+
+def face_reco_process(video, face_locations, face_encodings, known_faces, known_names):
+    # Capture a frame from the video
+    ret, frame = video.read()
+
+    process_this_frame = True
+
     if process_this_frame:
-        # Resize frame of video to 1/4 size for faster face recognition processing
+        # Resize the frame for faster processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])  # Convert to RGB format
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        # rgb_small_frame = small_frame[:, :, ::-1]
-        rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
-
-        # Find all the faces and face encodings in the current frame of video
+        # Locate faces in the frame
         face_locations = face_recognition.face_locations(rgb_small_frame)
+        # Calculate encodings of detected faces
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        face_names = []
-        
+        face_names = []  # List of detected face names
+
         for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
+            # Compare encodings of detected faces with known faces
             matches = face_recognition.compare_faces(known_faces, face_encoding)
-            name = "Unknown"
+            name = "Unknown"  # Default name if the face is not recognized
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
-            # Or instead, use the known face with the smallest distance to the new face
+            # Calculate the "distance" between known and detected faces
             face_distances = face_recognition.face_distance(known_faces, face_encoding)
-            best_match_index = np.argmin(face_distances)
+            best_match_index = np.argmin(face_distances)  # Select the closest match
+            # Assign names to detected faces
             if matches[best_match_index]:
-                name = known_names[best_match_index]
-
+                name = known_names[best_match_index]  # Name of the recognized face
             face_names.append(name)
 
-    process_this_frame = not process_this_frame
+    process_this_frame = not process_this_frame  # Toggle the frame processing indicator for the next iteration
 
-
-    # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        # Resize face locations to the original frame size
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
 
-        # Draw a box around the face
+        # Draw a bounding box around the face
         if name == "Unknown":
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-                    # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)  # Red box for unrecognized faces
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         else:
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, ), 2)
-                    # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)  # Green box for recognized faces
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
 
-
-
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)  # Display the face's name
 
-    # Display the resulting image
+    # Display the resulting image with detected faces
     cv2.imshow('Video', frame)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
--- distance_VL530X/distance_VL530X.ino
+++ distance_VL530X/distance_VL530X.ino
@@ -1,31 +1,86 @@
+/*
+  ----------------------------------------------------------------------------
+  File:       VL53L0X_Distance_Sensor.ino
+  Description: This Arduino sketch interfaces with the VL53L0X Time-of-Flight
+               distance sensor using the Adafruit VL53L0X library. It measures
+               distances and displays them over a serial communication interface.
+  ----------------------------------------------------------------------------
+  Author:     Clément Poirié
+  Date:       08/11/2000
+  Version:    1.0
+  ----------------------------------------------------------------------------
+  Usage:
+  - Ensure that the required Adafruit_VL53L0X library is installed in your Arduino IDE.
+  - Connect the VL53L0X sensor to your Arduino board according to the wiring instructions.
+  - Upload this sketch to your Arduino.
+  - Open the serial monitor to view distance measurements in millimeters.
+  ----------------------------------------------------------------------------
+*/
+
 #include <Wire.h>
 #include <Adafruit_VL53L0X.h>
 
 Adafruit_VL53L0X vl53 = Adafruit_VL53L0X();
 
 void setup() {
-  Serial.begin(115200); // Initialise la communication série pour afficher les résultats
-  Wire.begin(); // Initialise la liaison I2C
-  
+  Serial.begin(115200); // Initialize serial communication to display results
+  Wire.begin(); // Initialize the I2C communication
+
   if (!vl53.begin()) {
-    Serial.println(F("Échec de l'initialisation du VL53L0X"));
+    Serial.println(F("VL53L0X Initialization Failed"));
     while (1);
   }
 }
 
 void loop() {
-  VL53L0X_RangingMeasurementData_t measure; // initialise un struct pour stocker les données mesurées par notre capteur 
-    
-  if (vl53.getSingleRangingMeasurement(&measure)) { // Effectue une mesure unique de la distance retourne true si echec et false si réussi
+  VL53L0X_RangingMeasurementData_t measure; // Initialize a struct to store data measured by our sensor
 
-    Serial.println(F("Échec de la mesure de distance")); 
+  if (vl53.getSingleRangingMeasurement(&measure)) { // Perform a single distance measurement, returns true on failure, false on success
+
+    Serial.println(F("Distance Measurement Failed"));
 
   } else {
     Serial.print(F("Distance: "));
-    Serial.print(measure.RangeMilliMeter); // récupère la mesure dans le struct en milimetre
+    Serial.print(measure.RangeMilliMeter); // Retrieve the measurement from the struct in millimeters
     Serial.println(F(" mm"));
   }
 
-  // Attendez un court délai entre les lectures (facultatif)
+  // Wait for a short delay between readings (optional)
   delay(100);
 }
+
+
+
+
+
+// #include <Wire.h>
+// #include <Adafruit_VL53L0X.h>
+
+// Adafruit_VL53L0X vl53 = Adafruit_VL53L0X();
+
+// void setup() {
+//   Serial.begin(115200); // Initialise la communication série pour afficher les résultats
+//   Wire.begin(); // Initialise la liaison I2C
+  
+//   if (!vl53.begin()) {
+//     Serial.println(F("Échec de l'initialisation du VL53L0X"));
+//     while (1);
+//   }
+// }
+
+// void loop() {
+//   VL53L0X_RangingMeasurementData_t measure; // initialise un struct pour stocker les données mesurées par notre capteur 
+    
+//   if (vl53.getSingleRangingMeasurement(&measure)) { // Effectue une mesure unique de la distance retourne true si echec et false si réussi
+
+//     Serial.println(F("Échec de la mesure de distance")); 
+
+//   } else {
+//     Serial.print(F("Distance: "));
+//     Serial.print(measure.RangeMilliMeter); // récupère la mesure dans le struct en milimetre
+//     Serial.println(F(" mm"));
+//   }
+
+//   // Attendez un court délai entre les lectures (facultatif)
+//   delay(100);
+// }
