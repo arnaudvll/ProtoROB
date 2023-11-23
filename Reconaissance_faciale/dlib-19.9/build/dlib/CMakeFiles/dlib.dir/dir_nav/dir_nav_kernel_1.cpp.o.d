05576c6030c14913f25b3a63b7d6c1e68daaebd7
Clément Poirié <clementpoirie.pro@gmail.com>
2023-11-23 17:43:50 +0100

    folder clean

--- Reconaissance_faciale/dlib-19.9/build/dlib/CMakeFiles/dlib.dir/dir_nav/dir_nav_kernel_1.cpp.o.d
+++
@@ -1,4 +0,0 @@
-dlib/CMakeFiles/dlib.dir/dir_nav/dir_nav_kernel_1.cpp.o: \
- /home/clement/Documents/CPE_Lyon/proto/ProtoROB/Reconaissance_faciale/dlib-19.9/dlib/dir_nav/dir_nav_kernel_1.cpp \
- /usr/include/stdc-predef.h \
- /home/clement/Documents/CPE_Lyon/proto/ProtoROB/Reconaissance_faciale/dlib-19.9/dlib/dir_nav/../platform.h
--- Reconaissance_faciale/dlib-19.9/build/dlib/CMakeFiles/dlib.dir/sockets/sockets_kernel_1.cpp.o.d
+++
@@ -1,4 +0,0 @@
-dlib/CMakeFiles/dlib.dir/sockets/sockets_kernel_1.cpp.o: \
- /home/clement/Documents/CPE_Lyon/proto/ProtoROB/Reconaissance_faciale/dlib-19.9/dlib/sockets/sockets_kernel_1.cpp \
- /usr/include/stdc-predef.h \
- /home/clement/Documents/CPE_Lyon/proto/ProtoROB/Reconaissance_faciale/dlib-19.9/dlib/sockets/../platform.h
